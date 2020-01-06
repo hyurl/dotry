6db1c8e545b2ff1938ef7ccd6d3ebaf53e7f3a86
Ayon Lee <i@hyurl.com>
2020-01-07 00:43:27 +0800

    add dotry.call() and dotry.apply()

--- src/index.ts
+++ src/index.ts
@@ -4,116 +4,24 @@ export default dotry;
 
 // Declarations should be ordered from complex to simple.
 
-function dotry<E = Error, R = any, A extends any[]= any[]>(
+function dotry<E = Error, R = any, A extends any[] = any[]>(
     fn: (...args: A) => AsyncIterableIterator<R>,
     ...args: A
 ): AsyncIterableIterator<[E, R]>;
-
-function dotry<E = Error, R = any, A extends any[]= any[]>(
+function dotry<E = Error, R = any, A extends any[] = any[]>(
     fn: (...args: A) => IterableIterator<R>,
     ...args: A
 ): IterableIterator<[E, R]>;
-
-function dotry<E = Error, R = any, A extends any[]= any[]>(
+function dotry<E = Error, R = any, A extends any[] = any[]>(
     fn: (...args: A) => Promise<R>,
     ...args: A
 ): Promise<[E, R]>;
-
-function dotry<E = Error, R = any, A extends any[]= any[]>(
+function dotry<E = Error, R = any, A extends any[] = any[]>(
     fn: (...args: A) => R,
     ...args: A
 ): [E, R];
-
-function dotry<E, R, A extends any[]>(
-    fn: (...args: A) => R,
-    ...args: A
-): [E, R] |
-    Promise<[E, R]> |
-    IterableIterator<[E, R]> |
-    AsyncIterableIterator<[E, R]> {
-
-    try {
-        let res = fn.apply(void 0, args);
-
-        // Implementation details should be ordered from complex to simple.
-
-        if (isAsyncGenerator(res)) {
-            return (async function* () {
-                let input: any;
-                let result: any;
-
-                // Use `while` loop instead of `for...of...` in order to
-                // retrieve the return value of a generator function.
-                while (true) {
-                    try {
-                        let {
-                            done,
-                            value
-                        } = await (<AsyncIterableIterator<any>>res).next(input);
-
-                        if (done) {
-                            result = value;
-                            break;
-                        } else {
-                            // Receive any potential input value that passed to
-                            // the outer `next()` call, and pass them to
-                            // `res.next()` in the next call.
-                            input = yield Promise.resolve([null, value]);
-                        }
-                    } catch (err) {
-                        // If any error occurs, yield that error as resolved and
-                        // break the loop immediately, indicating the process
-                        // if force broken.
-                        yield Promise.resolve([err, undefined]);
-                        break;
-                    }
-                }
-
-                return Promise.resolve([null, result]);
-            })() as AsyncIterableIterator<[E, R]>;
-        } else if (isGenerator(res)) {
-            return (function* () {
-                let input: any;
-                let result: any;
-
-                while (true) {
-                    try {
-                        let {
-                            done,
-                            value
-                        } = (<IterableIterator<any>>res).next(input);
-
-                        if (done) {
-                            result = value;
-                            break;
-                        } else {
-                            input = yield [null, value];
-                        }
-                    } catch (err) {
-                        yield [err, undefined];
-                        break;
-                    }
-                }
-
-                return [null, result];
-            })() as IterableIterator<[E, R]>;
-        } else if (typeof res.then === "function") {
-            res = res.then((value: any) => [null, value]);
-
-            // There is no guarantee that a promise-like object's `then()`
-            // method will always return a promise, to avoid any trouble, we
-            // need to do one more check.
-            if (typeof res.catch === "function") {
-                return res.catch((err: any) => [err, undefined]);
-            } else {
-                return res;
-            }
-        } else {
-            return [null, res];
-        }
-    } catch (err) {
-        return [err, undefined];
-    }
+function dotry(fn: Function, ...args: any[]) {
+    return dotry.apply(void 0, <any>fn, args) as any;
 }
 
 namespace dotry {
@@ -121,7 +29,7 @@ namespace dotry {
      * Calls a traditional Node.js error-first callback style function and returns
      * a promise wrapped on the result.
      */
-    export function promisify<E = Error, R = any, A extends any[]= any[]>(
+    export function promisify<E = Error, R = any, A extends any[] = any[]>(
         fn: (...args: any[]) => any,
         ...args: A
     ): Promise<[E, R]> {
@@ -143,4 +51,133 @@ namespace dotry {
             });
         });
     };
+
+    export function call<E = Error, R = any, A extends any[] = any[]>(
+        thisArg: any,
+        fn: (...args: A) => AsyncIterableIterator<R>,
+        ...args: A
+    ): AsyncIterableIterator<[E, R]>;
+    export function call<E = Error, R = any, A extends any[] = any[]>(
+        thisArg: any,
+        fn: (...args: A) => IterableIterator<R>,
+        ...args: A
+    ): IterableIterator<[E, R]>;
+    export function call<E = Error, R = any, A extends any[] = any[]>(
+        thisArg: any,
+        fn: (...args: A) => Promise<R>,
+        ...args: A
+    ): Promise<[E, R]>;
+    export function call<E = Error, R = any, A extends any[] = any[]>(
+        thisArg: any,
+        fn: (...args: A) => R,
+        ...args: A
+    ): [E, R];
+    export function call(thisArg: any, fn: Function, ...args: any[]) {
+        return apply(thisArg, <any>fn, args) as any;
+    }
+
+    export function apply<E = Error, R = any, A extends any[] = any[]>(
+        thisArg: any,
+        fn: (...args: A) => AsyncIterableIterator<R>,
+        args: A
+    ): AsyncIterableIterator<[E, R]>;
+    export function apply<E = Error, R = any, A extends any[] = any[]>(
+        thisArg: any,
+        fn: (...args: A) => IterableIterator<R>,
+        args: A
+    ): IterableIterator<[E, R]>;
+    export function apply<E = Error, R = any, A extends any[] = any[]>(
+        thisArg: any,
+        fn: (...args: A) => Promise<R>,
+        args: A
+    ): Promise<[E, R]>;
+    export function apply<E = Error, R = any, A extends any[] = any[]>(
+        thisArg: any,
+        fn: (...args: A) => R,
+        args: A
+    ): [E, R];
+    export function apply(thisArg: any, fn: Function, args: any[]) {
+        try {
+            let res = fn.apply(thisArg, args);
+
+            // Implementation details should be ordered from complex to simple.
+
+            if (isAsyncGenerator(res)) {
+                return (async function* () {
+                    let input: any;
+                    let result: any;
+
+                    // Use `while` loop instead of `for...of...` in order to
+                    // retrieve the return value of a generator function.
+                    while (true) {
+                        try {
+                            let {
+                                done,
+                                value
+                            } = await (<AsyncIterableIterator<any>>res).next(input);
+
+                            if (done) {
+                                result = value;
+                                break;
+                            } else {
+                                // Receive any potential input value that passed
+                                // to the outer `next()` call, and pass them to
+                                // `res.next()` in the next call.
+                                input = yield Promise.resolve([null, value]);
+                            }
+                        } catch (err) {
+                            // If any error occurs, yield that error as resolved
+                            // and break the loop immediately, indicating the
+                            // process is forced broken.
+                            yield Promise.resolve([err, undefined]);
+                            break;
+                        }
+                    }
+
+                    return Promise.resolve([null, result]);
+                })() as AsyncIterableIterator<any>;
+            } else if (isGenerator(res)) {
+                return (function* () {
+                    let input: any;
+                    let result: any;
+
+                    while (true) {
+                        try {
+                            let {
+                                done,
+                                value
+                            } = (<IterableIterator<any>>res).next(input);
+
+                            if (done) {
+                                result = value;
+                                break;
+                            } else {
+                                input = yield [null, value];
+                            }
+                        } catch (err) {
+                            yield [err, undefined];
+                            break;
+                        }
+                    }
+
+                    return [null, result];
+                })() as IterableIterator<any>;
+            } else if (typeof res.then === "function") {
+                res = res.then((value: any) => [null, value]);
+
+                // There is no guarantee that a promise-like object's `then()`
+                // method will always return a promise, to avoid any trouble, we
+                // need to do one more check.
+                if (typeof res.catch === "function") {
+                    return res.catch((err: any) => [err, undefined]);
+                } else {
+                    return res;
+                }
+            } else {
+                return [null, res];
+            }
+        } catch (err) {
+            return [err, undefined];
+        }
+    }
 }
